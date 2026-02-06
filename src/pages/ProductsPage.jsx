c1a8a7aeebb749170ee9de436eab60e1973a3b8f
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-02-06 16:38:59 +0100

    correzione

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useGlobal } from "../context/GlobalContext";
 import SingleProductCard from "../components/SingleProductCard";
-import SingleProductList from "../components/SigleProductList";
+import SingleProductList from "../components/SingleProductList";
 
 
 export default function ProductsPage() {
